fdd6912c7e6953d3fd1627350620c5c3193bf20b
root <root@docker-discount-notifier.server>
2023-07-17 21:16:38 +0000

    Remove newline

--- DeployDockerMessageScheduler.sh
+++ DeployDockerMessageScheduler.sh
@@ -2,4 +2,3 @@
 
 docker build -t boascontainers/store-discount-message-scheduler:latest -f ./MessageScheduler/Dockerfile .
 docker image push boascontainers/store-discount-message-scheduler:latest
-
