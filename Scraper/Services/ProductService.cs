03ad00ff9bf15c87db0bdc1ab121d5683db3e856
Boas van der Hoeven <boas181@hotmail.com>
2023-07-18 22:03:20 +0200

    Revert static change

--- Scraper/Services/ProductService.cs
+++ Scraper/Services/ProductService.cs
@@ -17,7 +17,7 @@ public sealed class ProductService
 
     public async Task<string> AddProductFromMessage(string message, long userId)
     {
-        var url = UrlExtractorService.ExtractUrlFromMessage(message);
+        var url = urlExtractorService.ExtractUrlFromMessage(message);
         if (url is null)
         {
             return $"{message} did not contain a valid url.";
