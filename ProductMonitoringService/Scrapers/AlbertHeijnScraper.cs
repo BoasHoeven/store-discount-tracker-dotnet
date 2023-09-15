0708e304c7fb1e095ed6679b59e739440fe4ed85
Boas van der Hoeven <boas181@hotmail.com>
2023-09-15 22:43:33 +0200

    Update albert heijn

--- ProductMonitoringService/Scrapers/AlbertHeijnScraper.cs
+++ ProductMonitoringService/Scrapers/AlbertHeijnScraper.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
@@ -6,11 +8,13 @@ using ProductMonitoringService.Contracts;
 
 namespace ProductMonitoringService.Scrapers;
 
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 public sealed class AlbertHeijnScraper : IStoreScraper
 {
     private readonly HttpClient client;
     private readonly IMemoryCache cache;
     private readonly ILogger<AlbertHeijnScraper> logger;
+    private readonly StringBuilder cookieBuilder = new();
 
     public AlbertHeijnScraper(IHttpClientFactory clientFactory, IMemoryCache cache, ILogger<AlbertHeijnScraper> logger)
     {
@@ -23,12 +27,47 @@ public sealed class AlbertHeijnScraper : IStoreScraper
         client = clientFactory.CreateClient("AlbertHeijnClient");
     }
 
+    private static string CookieStripper(string cookie)
+    {
+        return $"{cookie.Split(';').First()};";
+    }
+
+    private async Task EnsureCookiesAsync()
+    {
+        if (cache.TryGetValue("CookieData", out string cachedCookie))
+        {
+            cookieBuilder.Clear();
+            cookieBuilder.Append(cachedCookie);
+            return;
+        }
+
+        var response = await client.GetAsync("https://www.ah.nl/");
+        if (response.Headers.TryGetValues("Set-Cookie", out var cookies))
+        {
+            var cookie = cookies.First(x => x.Contains("Ahonlnl-Prd-01"));
+            cookieBuilder.Clear();
+            // for (var i = 7; i <= 10; i++)
+            // {
+            //     var cookie = CookieStripper(cookies.ElementAt(i));
+            //     cookieBuilder.Append(cookie);
+            // }
+            cookieBuilder.Append(cookie);
+
+            cache.Set("CookieData", cookieBuilder, TimeSpan.FromMinutes(5));
+            logger.LogInformation("Cookies initialized: {Cookies}", cookieBuilder);
+        }
+        else
+        {
+            logger.LogWarning("Failed to initialize cookies. StatusCode: {StatusCode}", response.StatusCode);
+        }
+    }
+
     public async Task<IProduct?> GetProductFromId(string id, long userId)
     {
         var productDetails = await GetProductDetailsFromId(id);
 
         if (productDetails == null) return null;
-        
+
         IProduct product = new Product(id, productDetails.title, productDetails.price.unitSize,"Albert Heijn")
         {
             Price = productDetails.price.was ?? productDetails.price.now,
@@ -58,7 +97,7 @@ public sealed class AlbertHeijnScraper : IStoreScraper
         var discount = productDetails.discount;
         var tieredOffer = discount.tieredOffer?.Length > 0 ? string.Join(" & ", discount.tieredOffer): "";
         var discountMessage = tieredOffer != string.Empty ? tieredOffer : productDetails.shield?.text;
-        
+
         var productDiscount = new ProductDiscount
         {
             Product = product,
@@ -76,10 +115,13 @@ public sealed class AlbertHeijnScraper : IStoreScraper
 
     private async Task<ProductDetails?> GetProductDetailsFromId(string id)
     {
+        await EnsureCookiesAsync();
+        client.DefaultRequestHeaders.Add("Cookie", cookieBuilder.ToString());
+
         var response = await client.GetAsync($"zoeken/api/products/product?webshopId={id}");
 
         if (!response.IsSuccessStatusCode) return null;
-        
+
         var jsonResponse = await response.Content.ReadAsStringAsync();
 
         var root = JsonSerializer.Deserialize<RootObject>(jsonResponse);
